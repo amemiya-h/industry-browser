aeffcd53d8b571f38aa0a9852fd7b26b814023c6
amemiya-h <aozora.kei.works@gmail.com>
2025-03-23 04:47:13 +0900

    Finishing touches

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Header from "./components/Header.tsx";
 import Viewport from "./components/Viewport.tsx";
 import './App.css'
 import {ViewportProvider} from "./components/ViewportContext.tsx";
+import {BrowserRouter} from "react-router-dom";
 
 const App = ()=> {
   return (
     <div className="App flex flex-col content-center justify-start h-screen w-screen m-0 p-0">
-        <Header />
-        <ViewportProvider>
-            <Viewport />
-        </ViewportProvider>
+        <BrowserRouter>
+            <Header />
+            <ViewportProvider>
+                <Viewport />
+            </ViewportProvider>
+        </BrowserRouter>
     </div>
   )
 }
